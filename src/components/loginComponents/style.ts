74b3c097e11bb134ec0494152c685738c975f627
Jaqui-Rocha <131921790+Jaqui-Rocha@users.noreply.github.com>
2023-09-28 07:52:28 -0400

    .

--- src/components/loginComponents/style.ts
+++ src/components/loginComponents/style.ts
@@ -1,8 +1,13 @@
 import styled from 'styled-components'
-import Login from '@/assets/Login.png'
-
-export const backGroundImage = styled.div`
-  background-image: url(${Login});
+import fundo from '../../assets/Login.png'
+export const BackGround1 = styled.div`
+  background: url(${fundo});
   width: auto;
   height: 100vh;
+  border: 1px solid green;
+
+  html {
+    margin: 0;
+    padding: 0;
+  }
 `
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import Input from '@/components/ui/uiForm/input'
 import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai'
 import { Container, Global, BemVindo } from '@/assets/global'
-import { backGroundImage } from '@/components/loginComponents/style'
+import { BackGround1 } from '@/components/loginComponents/style'
+import Login from '../assets/Login.png'
 import {
   FormGroup,
   FormControlLabel,
@@ -14,6 +15,7 @@ import StyleInput from '@/components/ui/uiForm/styleInput'
 import { StyleCheck } from '@/components/ui/uiForm/styleMaterial'
 import { Button } from '@/components/ui/uiForm/button'
 import { useNavigate } from 'react-router-dom'
+import { BackHandOutlined } from '@mui/icons-material'
 function PageLogin() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -26,44 +28,45 @@ function PageLogin() {
   }
   return (
     <>
-      {/* <backGroundImage /> */}
-      <Container>
-        <BemVindo>
-          <p>Seja bem vindo!</p>
-          <h1>Realize seu Login</h1>
-        </BemVindo>
-        <Input
-          id="email"
-          label="Email"
-          type="email"
-          onChange={e => setEmail(e.target.value)}
-          value={email}
-          hasError={!emailRegex.test(email)}
-          placeholder="Insira seu Email"
-        />
-        <Input
-          id="password"
-          label="Senha"
-          type={showPassword ? 'text' : 'password'}
-          onChange={e => setPassword(e.target.value)}
-          value={password}
-          icon={showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
-          onIcon={() => setShowPassword(!showPassword)}
-          placeholder="Insira sua Senha"
-        />
-        <StyleCheck>
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox defaultChecked />}
-              label="Lembrar-me"
-            />{' '}
-          </FormGroup>
-          Esqueci minha senha
-        </StyleCheck>
-        <Button $primary onClick={handlePage}>
-          Entrar
-        </Button>
-      </Container>
+      <BackGround1>
+        <Container>
+          <BemVindo>
+            <p>Seja bem vindo!</p>
+            <h1>Realize seu Login</h1>
+          </BemVindo>
+          <Input
+            id="email"
+            label="Email"
+            type="email"
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+            hasError={!emailRegex.test(email)}
+            placeholder="Insira seu Email"
+          />
+          <Input
+            id="password"
+            label="Senha"
+            type={showPassword ? 'text' : 'password'}
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+            icon={showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+            onIcon={() => setShowPassword(!showPassword)}
+            placeholder="Insira sua Senha"
+          />
+          <StyleCheck>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox defaultChecked />}
+                label="Lembrar-me"
+              />{' '}
+            </FormGroup>
+            Esqueci minha senha
+          </StyleCheck>
+          <Button $primary onClick={handlePage}>
+            Entrar
+          </Button>
+        </Container>
+      </BackGround1>
       <Global />
     </>
   )
