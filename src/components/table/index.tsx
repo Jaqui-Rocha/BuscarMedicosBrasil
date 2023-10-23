0ea18759c3841186ecf060c8520c59b1fc733683
Jaqui-Rocha <131921790+Jaqui-Rocha@users.noreply.github.com>
2023-10-23 01:26:04 -0400

    .

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,30 +1,29 @@
 import { ReactNode } from 'react'
 import { Table, Header, HeaderData } from './styleTable'
-//type TableRow = Record<string, string | number | Array<string> | ReactNode>
+type TableRow = Record<string, string | number | Array<string> | ReactNode>
 
 type HeadColumns = {}
 type TableProps = {
   HeadColumns: string[]
   BodyRow: string[] | number | ReactNode
 }
-
 export const TableComponent = ({ HeadColumns, BodyRow }: TableProps) => {
   return (
     <>
       <Table>
-        <Header>
+        <thead>
           <tr>
             {HeadColumns.map((columnText, index) => (
-              <HeaderData key={`${index}-th`}>{columnText}</HeaderData>
+              <th key={`${index}-th`}>{columnText}</th>
             ))}
           </tr>
-        </Header>
+        </thead>
         <tbody>
-          {BodyRow.map((BodyRow, index) => {
+          {BodyRow.map((information, index) => {
             return (
               <>
                 <tr key={`${index}-tr`}>
-                  {BodyRow.map((cell, index) => (
+                  {Object.values(information).map((cell, index) => (
                     <>
                       <td key={`${index}-td`}>{cell}</td>
                     </>
